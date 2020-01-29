6745b15e6545d756fc55f2686b72493062bad131
Christian Cormier <ccpokemon@yahoo.com>
2020-01-29 13:14:32 -0600

    wip attack

--- Weapon.java
+++ Weapon.java
@@ -1,1 +1,27 @@
+import java.util.*;
+public class Weapon{
+    private int dmg;
+    private String dmgType;
+    private double crit;
+    private String WEAPON_TYPE;
 
+    public Weapon(int dmg, String dmgType, double crit){
+        this.dmg = dmg;
+        this.dmgType = dmgType;
+        this.crit = 2.0;
+    }
+
+    public int getDmg(){
+        return this.dmg;
+    }
+
+    public double crit(){
+        return this.crit;
+    }
+    public int attack(Character enemy){
+        int damage = (int) (Math.random() * dmg) + (int) (.5 * dmg);
+        if(enemy.isWeakTo(dmgType)){
+            damage += .5 * damage;
+        }
+    }
+}
