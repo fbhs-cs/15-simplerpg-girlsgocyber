07a3c5691014d38f457f2b80649582f095d79efd
helenasimmons <helenacctx@gmail.com>
2020-01-30 13:16:12 -0600

    working on TestCharacter

--- TestCharacter.java
+++ TestCharacter.java
@@ -1,6 +1,10 @@
 public class TestCharacter{
 
-    public static int testGetHP(Character c){
+    public static void testIsWeakTo(Character c){
+        
+    }
+
+    public static void testGetHP(Character c){
         System.out.println(c.getHP());
     }
 
@@ -9,11 +13,30 @@ public class TestCharacter{
 
     }
 
+    public static void testAddXP(Character c){
+
+    }
+
+    public static void testGetXP(Character c){
+
+    }
+
+
+    public static void testSetLevel(Character c){
+        c.setLevel(9001);
+    }
+
+    public static void testGetLevel(Character c){
+        System.out.println(c.getLevel());
+    }
+
     public static void main(String[] args) {
         Character bob = new Character("Bob", "Love", 7);
         testGetHP(bob);
         testSetHP(bob);
         testGetHP(bob);
+        testSetHP(bob);
+        testGetLevel(bob);
         
 
         
