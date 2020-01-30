47f6530f7245ca93fdffc45b05cac26e7aaac91f
helenasimmons <helenacctx@gmail.com>
2020-01-30 12:58:21 -0600

    working on TestCharacter

--- TestCharacter.java
+++ TestCharacter.java
@@ -1,1 +1,24 @@
+public class TestCharacter{
 
+    public static int testGetHP(Character c){
+        System.out.println(c.getHP());
+    }
+
+    public static void testSetHP(Character c){
+        c.setHP(10);
+
+    }
+
+    public static void main(String[] args) {
+        Character bob = new Character("Bob", "Love", 7);
+        testGetHP(bob);
+        testSetHP(bob);
+        testGetHP(bob);
+        
+
+        
+        
+
+    }
+
+}
