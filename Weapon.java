2f4f4335f23ee1df1279871e269c4cab97d75ce4
Christian Cormier <ccpokemon@yahoo.com>
2020-01-30 13:10:58 -0600

    fin weapon

--- Weapon.java
+++ Weapon.java
@@ -8,20 +8,36 @@ public class Weapon{
     public Weapon(int dmg, String dmgType, double crit){
         this.dmg = dmg;
         this.dmgType = dmgType;
-        this.crit = 2.0;
+        this.crit = .10;
     }
 
     public int getDmg(){
         return this.dmg;
     }
 
-    public double crit(){
+    public double getCrit(){
         return this.crit;
+    }
+
+    public String getDmgType(){
+        return this.dmgType;
+    }
+
+    public String getWeaponType(){
+        return this.WEAPON_TYPE;
+    }
+
+    public String to_String(){
+        return String.format("Weapon: %s, damage: %d, damage type: %s",WEAPON_TYPE,dmg,dmgType);
     }
     public int attack(Character enemy){
         int damage = (int) (Math.random() * dmg) + (int) (.5 * dmg);
         if(enemy.isWeakTo(dmgType)){
             damage += .5 * damage;
         }
+        if(Math.random() < getCrit()){
+            damage *= 2;
+        }
+        return damage;
     }
 }
