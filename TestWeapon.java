1f1cd2376600e280f3a0161124e0511dc3206856
rachelh2071 <rachelh2071@gamil.com>
2020-01-30 13:13:23 -0600

    started tester

---
+++ TestWeapon.java
@@ -0,0 +1,32 @@
+public class TestWeapon{
+
+    // public static void testAttack(){
+
+
+    //     foot.attack(bob);
+    //     System.out
+    // }
+
+    // public static void testGetDmg(){
+    //     Weapon foot = new Weapon(3,"Love", .10);
+    //     Character bob = new Character("Bob","Love",7)
+    //     foot.attack(bob);
+    // }
+    // public static void testAttack(){
+    //     Weapon foot = new Weapon(3,"Love", .10);
+    //     Character bob = new Character("Bob","Love",7)
+    //     foot.attack(bob);
+    }
+    public static void main(String[] args){
+        Weapon foot = new Weapon(3,"Love", .10);
+        Character bob = new Character("Bob","Love",7);
+        foot.attack(bob);
+        foot.getWeaponType();
+        foot.getDmgType();
+        foot.getDmg();
+        foot.getCrit();
+        System.out.print(foot);
+
+
+    }
+}
