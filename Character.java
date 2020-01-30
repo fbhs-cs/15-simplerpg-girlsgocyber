fd2d0c533875769dcbc9658510a32b1aef6c740a
rachelh2071 <rachelh2071@gamil.com>
2020-01-30 12:54:47 -0600

    i think i finished

--- Character.java
+++ Character.java
@@ -1,19 +1,21 @@
 import java.util.*;
 import java.lang.*;
 public class Character{
-    private int hp,xp,speed;
+    private int hp,xp,speed,mana;
     private String name;
-    private String CLASS_NAME;
+    private static String CLASS_NAME;
     // private Weapon weapon;
     // private Spell spell;
     private boolean hasWeapon, hasSpell;
     private String weakness;
 
-    public Character(String name, String[] weakness, int speed){
+
+    public Character(String name, String weakness, int speed){
         this.name = name;
         this.weakness = weakness;
         this.speed = speed;
     }
+
     
     public int getHP(){
         return hp;
@@ -22,6 +24,15 @@ public class Character{
     public int getXP(){
         return xp;
     }
+
+    public int getMana(){
+        return mana;
+    }
+
+    public void setMana(int mana){
+        this.mana = mana;
+    }
+
 
     public void setHP(int hp){
         this.hp = hp;
@@ -39,12 +50,45 @@ public class Character{
         return this.getLevel()*12;
     }
 
+    public void takeDmg(int dmg){
+        setHP(getHP()-dmg);
+    }
+
+    public boolean isAlive(){
+        if(getHP()<=0){
+            return false;
+        }
+
+        else{
+            return true;
+        }
+    }
+
+
+    public boolean isWeakTo(String strength){
+        if(weakness.equals(strength)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     public String getName(){
         return name;
     }
 
-    public void attack(Character target){
-        System.out.print("I bite my thumb at you");
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void chooseAction(Character target){
+        target.takeDmg(5);
+        System.out.printf("%s uses the popular masses to harry %s",name,target.getName());
+    }
+
+    public String toString(){
+        return String.format("%s : %s Level : %d HP : %d",CLASS_NAME,getName(), getLevel(), getHP());
     }
 
 
