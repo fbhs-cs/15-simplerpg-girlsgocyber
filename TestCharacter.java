5eb3746cda103519989739b4f33048ad417efa04
helenasimmons <helenacctx@gmail.com>
2020-01-31 12:41:58 -0600

    working on TestCharacter

--- TestCharacter.java
+++ TestCharacter.java
@@ -25,13 +25,23 @@ public class TestCharacter{
         Character bob = new Character("Bob", "Love", 7);
         bob.setHP(1000);
         bob.setLevel(123);
+        bob.setMana(50);
         // bob.addXP(5); DOESN'T WORK
         
-
+        System.out.println(bob.toString());
+        System.out.println();
+        System.out.println("Name: " + bob.getName());
         System.out.println("Level: " + bob.getLevel());
         System.out.println("Test Weakness: " + bob.isWeakTo("Love")); 
         System.out.println("Health: " + bob.getHP());
         System.out.println("XP: " + bob.getXP());
+        System.out.println("Get Speed: " + bob.getSpeed());
+
+        bob.takeDmg(500);
+        System.out.println("Take 500 dmg: " + bob.getHP());
+        System.out.println("Alive? " + bob.isAlive());
+        System.out.println("Mana: " + bob.getMana());
+
         
 
         
