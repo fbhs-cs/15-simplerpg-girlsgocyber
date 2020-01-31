1c1dea2bab9b2c25b9af4fc95cbf34daee2c2840
helenasimmons <helenacctx@gmail.com>
2020-01-31 12:37:38 -0600

    added ()

--- TestCharacter.java
+++ TestCharacter.java
@@ -1,42 +1,37 @@
 public class TestCharacter{
 
-    public static void testIsWeakTo(Character c){
+    // public static void testIsWeakTo(Character c){
         
-    }
+    // }
 
-    public static void testGetHP(Character c){
-        System.out.println(c.getHP());
-    }
+    // public static void testAddXP(Character c){
 
-    public static void testSetHP(Character c){
-        c.setHP(10);
+    // }
 
-    }
+    // public static void testGetXP(Character c){
 
-    public static void testAddXP(Character c){
-
-    }
-
-    public static void testGetXP(Character c){
-
-    }
+    // }
 
 
-    public static void testSetLevel(Character c){
-        c.setLevel(9001);
-    }
+    // public static void testSetLevel(Character c){
+    //     c.setLevel(9001);
+    // }
 
-    public static void testGetLevel(Character c){
-        System.out.println(c.getLevel());
-    }
+    // public static void testGetLevel(Character c){
+    //     System.out.println(c.getLevel());
+    // }
 
     public static void main(String[] args) {
         Character bob = new Character("Bob", "Love", 7);
-        testGetHP(bob);
-        testSetHP(bob);
-        testGetHP(bob);
-        testSetHP(bob);
-        testGetLevel(bob);
+        bob.setHP(1000);
+        bob.setLevel(123);
+        // bob.addXP(5); DOESN'T WORK
+        
+
+        System.out.println("Level: " + bob.getLevel());
+        System.out.println("Test Weakness: " + bob.isWeakTo("Love")); 
+        System.out.println("Health: " + bob.getHP());
+        System.out.println("XP: " + bob.getXP());
         
 
         
