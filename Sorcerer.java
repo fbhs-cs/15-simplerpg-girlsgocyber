ae6ddd6cfb3d9e8245125169bf04338902e8b99d
helenasimmons <helenacctx@gmail.com>
2020-02-05 13:19:28 -0600

    need to work on random choices for enemies

--- Sorcerer.java
+++ Sorcerer.java
@@ -10,25 +10,27 @@ public class Sorcerer extends Character{
     }
 
     public void chooseAction(Character target){
-        Scanner options = new Scanner(System.in);
-        int choice = 0;
-        while(true){
-            try{
-        System.out.println("What would you like to do? ");
-        System.out.println("1. Summon a Dragon");
-        System.out.println("2. Hydrate Before You Diedrate (+ 10 Mana)");
-        System.out.print("> ");
-        choice = Integer.parseInt(options.nextLine());
-        if(choice < 1 || choice > 2) {
-            System.out.println("You must type in 1 or 2");
-            continue;
-        }
-        break;
 
-        } catch (Exception e) {
-            System.out.println("You must type in 1 or 2");
-        }
-        options.close();
+        int choice = 0;
+        // Scanner options = new Scanner(System.in);
+        // int choice = 0;
+        // while(true){
+        //     try{
+        // System.out.println("What would you like to do? ");
+        // System.out.println("1. Summon a Dragon");
+        // System.out.println("2. Hydrate Before You Diedrate (+ 10 Mana)");
+        // System.out.print("> ");
+        // choice = ;
+        // if(choice < 1 || choice > 2) {
+        //     System.out.println("You must type in 1 or 2");
+        //     continue;
+        // }
+        // break;
+
+        // } catch (Exception e) {
+        //     System.out.println("You must type in 1 or 2");
+        // }
+        // options.close();
 
         if(choice==1){
             target.takeDmg(spell.cast(this,target));
