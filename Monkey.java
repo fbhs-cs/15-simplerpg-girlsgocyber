e7b68e6964f63a548682045c8012d885b5186787
helenasimmons <helenacctx@gmail.com>
2020-02-06 12:44:40 -0600

    fixed parenthesis

--- Monkey.java
+++ Monkey.java
@@ -43,7 +43,7 @@ public class Monkey extends Character{
 
 
     public String toString(){
-        return name;
+        return getName();
     }
 
 
--- Sorcerer.java
+++ Sorcerer.java
@@ -12,25 +12,6 @@ public class Sorcerer extends Character{
     public void chooseAction(Character target){
 
         int choice = (int)(Math.random()*2)+1;
-        // Scanner options = new Scanner(System.in);
-        // int choice = 0;
-        // while(true){
-        //     try{
-        // System.out.println("What would you like to do? ");
-        // System.out.println("1. Summon a Dragon");
-        // System.out.println("2. Hydrate Before You Diedrate (+ 10 Mana)");
-        // System.out.print("> ");
-        // choice = ;
-        // if(choice < 1 || choice > 2) {
-        //     System.out.println("You must type in 1 or 2");
-        //     continue;
-        // }
-        // break;
-
-        // } catch (Exception e) {
-        //     System.out.println("You must type in 1 or 2");
-        // }
-        // options.close();
 
         if(choice==1){
             System.out.println("Old Guy Sorcerer has summoned a dragon!"));
