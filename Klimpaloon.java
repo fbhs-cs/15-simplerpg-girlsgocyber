510abc6aa3d290d03e89822f3b9078c938bec768
rachelh2071 <43217928+rachelh2071@users.noreply.github.com>
2020-02-05 21:01:54 -0600

    changed chooseaction to be random

--- Klimpaloon.java
+++ Klimpaloon.java
@@ -11,25 +11,25 @@ public class Klimpaloon extends Character{
     }
 
     public void chooseAction(Character target){
-        Scanner options = new Scanner(System.in);
-        int choice = 0;
-        while(true){
-            try{
-        System.out.println("What would you like to do? ");
-        System.out.println("1. Attack with ----");
-        System.out.println("2. Taunt");
-        System.out.print("> ");
-        choice = Integer.parseInt(options.nextLine());
-        if(choice == 1 || choice == 2) {
-            System.out.println("You must type in 1 or 2");
-            continue;
-        }
-        break;
+//         Scanner options = new Scanner(System.in);
+        int choice = (int)(Math.random()*2)+1;
+//         while(true){
+//             try{
+//         System.out.println("What would you like to do? ");
+//         System.out.println("1. Attack with ----");
+//         System.out.println("2. Taunt");
+//         System.out.print("> ");
+//         choice = Integer.parseInt(options.nextLine());
+//         if(choice == 1 || choice == 2) {
+//             System.out.println("You must type in 1 or 2");
+//             continue;
+//         }
+//         break;
 
-    } catch (Exception e) {
-        System.out.println("You must type in 1 or 2");
-    }
-    options.close();
+//     } catch (Exception e) {
+//         System.out.println("You must type in 1 or 2");
+//     }
+//     options.close();
 
     if(choice==1){
         weapon.attack(target);
