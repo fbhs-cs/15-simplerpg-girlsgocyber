1a8a1fa1752feccae1bc8b5ee17f72036cf24da0
Christian Cormier <ccpokemon@yahoo.com>
2020-02-06 12:36:31 -0600

    fix winnie

--- Winnie.java
+++ Winnie.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 public class Winnie extends Character{
     private Spell spell;
 
@@ -9,33 +8,13 @@ public class Winnie extends Character{
     }
 
     public void chooseAction(Character enemy){
-        Scanner options = new Scanner(System.in);
-        int choice = 0;
-        while(true){
-            try{
-                System.out.println("What would you like to do? ");
-                System.out.println("1. Get a perm (+5 health)");
-                System.out.println("2. attack with your weapon");
-                System.out.print("> ");
-                choice = Integer.parseInt(options.nextLine());
-                if(choice < 1 || choice > 2) {
-                    System.out.println("You must type in 1 or 2");
-                    continue;
-                }
-                break;
-
-            } 
-            catch (Exception e) {
-                System.out.println("You must type in 1 or 2");
-            }
-            options.close();
-
-            if(choice==2){
-                enemy.takeDmg(weapon.attack(enemy));
-            }
-            else{
-                setHP(getMaxHP() + 5);
-            }
+        int choice = (int)(Math.random()*2)+1;
+        if(choice==2){
+            enemy.takeDmg(spell.cast(this,enemy));
+        }
+        else{
+            setHP(getMaxHP() + 5);
         }
     }
+
 }
